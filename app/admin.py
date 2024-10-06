148c363416d542ef2a1ed2ca19779e9eeccfd000
Anurag Krishna <anuragraj09k@gmail.com>
2024-10-06 10:47:50 +0530

    Sample Commit

---
+++ app/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from . models import Customer, Product
+
+# Register your models here.
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ['id','title','discounted_price','product_image']
+
+@admin.register(Customer)
+class CustomerModelAdmin(admin.ModelAdmin):
+    list_display = ['id','user','locality','city','state','zipcode']   
